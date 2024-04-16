c8dde42142471a84a216cd7c6f931cd3d647c5a9
Kranthi Kumar <kranthi.panda@outlook.com>
2024-04-16 15:00:47 +0530

    Implement Data driven mechanism by removing hard coding data from tests

--- tests/test_HomePage.py
+++ tests/test_HomePage.py
@@ -1,7 +1,4 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
+import pytest
 
 from pageObjects.HomePage import HomePage
 from utilities.BaseClass import BaseClass
@@ -9,21 +6,24 @@ from utilities.BaseClass import BaseClass
 
 class TestHomePage(BaseClass):
 
-    def test_formSubmission(self):
-
+    def test_formSubmission(self, getData):
         homePage = HomePage(self.driver)
 
-        homePage.getName().send_keys("Panda")
-        homePage.getEmail().send_keys("hello@gmail.com")
+        homePage.getName().send_keys(getData[0])
+        homePage.getEmail().send_keys(getData[1])
         homePage.getPassword().send_keys("12345")
         homePage.getCheckbox().click()
         homePage.getRadioBtn().click()
 
-        self.selectDropDownByText(homePage.getGender(), "Female")
+        self.selectDropDownByText(homePage.getGender(), getData[2])
         self.selectDropDownByIndex(homePage.getGender(), 0)
 
         homePage.clickSubmitBtn()
         message = homePage.alertSuccessMessage()
-
-        print(message)
         homePage.validateMessage("Success", message)
+
+        self.driver.refresh()
+
+    @pytest.fixture(params=[("Kranthi", "Panda", "Male"), ("Meera", "Panda", "Female")])
+    def getData(self, request):
+        return request.param
